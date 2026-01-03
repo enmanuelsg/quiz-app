27a125238211c86d90d2e066e2ff0dc1d13a115f
enmanuelsg <enmanuel.s@gmail.com>
2026-01-03 23:06:57 +0100

    Add quiz feed API endpoint

--- quiz/urls.py
+++ quiz/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('rankings/', views.rankings_page, name='rankings_page'),
     path('api/rankings/', user_rankings, name='api_rankings'),
+    path('api/quiz-feed/', views.quiz_feed_api, name='quiz_feed_api'),
     path('logout/', views.logout_view, name='logout'),
     path('quiz_selection/', views.quiz_selection, name='quiz_selection'),
 
